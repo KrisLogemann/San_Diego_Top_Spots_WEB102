fc6d6487a55ae1bdc4c2984210782f3006bb6324
Kris Logemann <krisrlogemann@gmail.com>
2019-05-18 11:08:41 -0700

    adds initial project files

---
+++ README.md
@@ -0,0 +1,5 @@
+//San Diego Top Spots WEB102. Created on May 18, 2019 by Kris Logemann.
+
+Hey everybody, thanks for checking out my code in my Github.
+
+This is my thris project in the WEB102 section of my curriculum at San Diego Code School. This project was simply taking data.json data and turning it into a table, rather than having to go in and type everything out individually. The web app displays fun activities to do in San Diego with links for directions to get to any of the given locations.
--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-$(document).ready(function() {
-// write your code here
-
+$.getJSON("data.json", function(data) {
+    var topSpots = '';
+    $.each(data, function(key, value){
+        topSpots += '<tr>'
+        topSpots += '<td>'+value.name+'</td>';
+        topSpots += '<td>'+value.description+'</td>';
+        topSpots += '<td>'+'<a href='+'https://www.google.com/maps?q='+value.location+'>Click Here For Directions</a>'+'</td>';
+        topSpots += '</tr>';
+    });
+    $('#top-spots').append(topSpots);
 });
